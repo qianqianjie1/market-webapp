c6a67dd13254205951d8aecf4da115ba9ac5e0fc
qianqinajie1 <2253933765@qq.com>
2019-09-11 19:52:02 +0800

    移动端适配

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -34,7 +34,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      '@': resolve('src'),
+      'api': resolve('src/api'),
+      'assets': resolve('src/assets'),
+      'base': resolve('src/base'),
+      'components': resolve('src/components'),
+      'pages': resolve('src/pages')
     }
   },
   module: {
